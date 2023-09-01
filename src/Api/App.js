0a3e50e33bc8813c8ae1226513f74ca7fda57f7b
tiramisa <miller19988@gmail.com>
2023-09-01 12:46:52 +0100

    фиксим пиксим

--- src/Api/App.js
+++ src/Api/App.js
@@ -1,16 +1,7 @@
-import axios from 'axios';
-
 const apiKey = 'c20148b967662b7bc56a396dddd785bb';
-const movieId = 1;
-const url = `${axios.defaults.baseURL}movie/${movieId}?api_key=${apiKey}`;
+const baseURL = 'https://api.themoviedb.org/3/';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Ошибка:', error);
-  });
+export function getPopularMoviesFromApi() {
+  const URL = `${baseURL}trending/all/day?language=en-US`;
+  fetch(``);
+}
