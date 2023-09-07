8cbc453b572a48f2b87fbbcbbbecb9c54e22c9f8
tiramisa <miller19988@gmail.com>
2023-09-07 13:12:05 +0100

    MovieItem

---
+++ src/components/MovieItem/MovieItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const MovieItem = ({ arrayOfMovies }) => {
+  console.log('arrayOfMovies', arrayOfMovies);
+  return <div></div>;
+};
+
+// MovieItem.propTypes = {};
+
+export default MovieItem;
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,18 +1,27 @@
 import { getPopularMoviesFromApi } from '../../../Api/App';
 import { useState, useEffect } from 'react';
+import MovieItem from '../../MovieItem/MovieItem';
 
 const Home = () => {
+  const [arrayOfMovies, setArrayOfMovies] = useState(null);
   useEffect(() => {
     getPopularMoviesFromApi()
       .then(data => {
         console.log(data);
+        setArrayOfMovies(data.results);
       })
       .catch(error => {
         console.error(error);
       });
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <>
+      {!arrayOfMovies && <p>nema foto!</p>}
+      {arrayOfMovies && <MovieItem arrayOfMovies={arrayOfMovies} />}
+      <div></div>
+    </>
+  );
 };
 
 export default Home;
