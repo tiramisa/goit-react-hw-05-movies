fecdf6b01becd8cf6ca6ec7a039cfe22a60c3cd6
tiramisa <miller19988@gmail.com>
2023-09-09 21:55:43 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,24 +6,51 @@ import {
   InstagramIcon,
   FacebookIcon,
   TwitterIcon,
+  FooterCopy,
+  FooterContent,
+  FooterText,
 } from './Footer.styled';
 
 const Footer = () => {
+  const handleInstagramClick = () => {
+    window.location.href = 'https://www.instagram.com/';
+  };
+
+  const handleFacebookClick = () => {
+    window.location.href = 'https://www.facebook.com/';
+  };
+
+  const handleTwitterClick = () => {
+    window.location.href = 'https://twitter.com/';
+  };
+
   return (
     <footer>
       <Container>
         <Social>
-          <InstagramIcon>
+          <InstagramIcon onClick={handleInstagramClick}>
             <FaInstagram />
           </InstagramIcon>
-          <FacebookIcon>
+          <FacebookIcon onClick={handleFacebookClick}>
             <FaFacebook />
           </FacebookIcon>
-          <TwitterIcon>
+          <TwitterIcon onClick={handleTwitterClick}>
             <FaTwitter />
           </TwitterIcon>
         </Social>
-        <p>&copy; SoftCat</p>
+        <FooterContent>
+          <FooterText>
+            We are SoftCat, your trusted source for everything related to the
+            film industry. We provide you with up-to-date information about new
+            films, reviews, interesting facts, and much more.
+          </FooterText>
+
+          <FooterText>
+            Follow us on Instagram, Facebook, and Twitter to keep up to date
+            with the latest updates and share your impressions about movies!
+          </FooterText>
+        </FooterContent>
+        <FooterCopy>&copy; SoftCat</FooterCopy>
       </Container>
     </footer>
   );
--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -9,6 +9,17 @@ export const Social = styled.div`
   margin: auto;
   padding: 20px;
   gap: 24px;
+  cursor: pointer;
+
+  & > svg {
+    transition: transform 0.3s ease-in-out, fill 0.3s ease-in-out;
+  }
+
+  & > svg:hover,
+  & > svg:focus {
+    fill: #ccc;
+    transform: scale(1.2);
+  }
 `;
 
 export const InstagramIcon = styled(FaInstagram)`
@@ -25,3 +36,21 @@ export const TwitterIcon = styled(FaTwitter)`
   width: 24px;
   height: 24px;
 `;
+export const FooterCopy = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  padding: 10px;
+`;
+export const FooterContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  margin: auto;
+  padding: 20px;
+  gap: 24px;
+`;
+export const FooterText = styled.p``;
