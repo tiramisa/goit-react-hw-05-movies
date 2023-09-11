7996e72f67d9685be4d5d520d44132a8492de009
tiramisa <miller19988@gmail.com>
2023-09-12 00:13:56 +0100

    Update MovieDetails.jsx

--- src/components/Pages/MovieDetails/MovieDetails.jsx
+++ src/components/Pages/MovieDetails/MovieDetails.jsx
@@ -7,7 +7,7 @@ import Loader from 'components/Loader/Loader';
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieInfo, setMovieInfo] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading] = useState(true);
 
   useEffect(() => {
     getMovies(movieId).then(data => {
