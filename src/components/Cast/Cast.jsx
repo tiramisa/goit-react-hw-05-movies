5ca5a150460cbf6a6b13ec9374419fd0903769a7
tiramisa <miller19988@gmail.com>
2023-09-11 23:12:44 +0100

    all loader

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -2,19 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMoviesCast } from 'Api/App';
 import ActorFoto from 'img/actorFoto.jpeg';
+import Loader from 'components/Loader/Loader';
 
 const Cast = () => {
   const { movieId } = useParams();
   const [cast, setCast] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getMoviesCast(movieId).then(data => {
-      setCast(data.cast);
-    });
+    getMoviesCast(movieId)
+      .then(data => {
+        setCast(data.cast);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching reviews:', error);
+        setLoading(false);
+      });
   }, [movieId]);
 
   return (
     <div>
+      {loading && <Loader />}
+
       {cast.length === 0 && <p>No cast available</p>}
 
       {cast && (
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMoviesReviews } from 'Api/App';
+import Loader from 'components/Loader/Loader';
 
 const Reviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getMoviesReviews(movieId)
       .then(data => {
         setReviews(data.results);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching reviews:', error);
+        setLoading(false);
       });
   }, [movieId]);
 
   return (
     <div>
+      {loading && <Loader />}
+
       {reviews.length === 0 ? (
         <p>No reviews available</p>
       ) : (
