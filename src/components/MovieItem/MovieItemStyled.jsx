d7a65d9992c8db11d30bcbfffd88ed489ba3b3d1
tiramisa <miller19988@gmail.com>
2023-09-12 02:04:39 +0100

    Update MovieItemStyled.jsx

--- src/components/MovieItem/MovieItemStyled.jsx
+++ src/components/MovieItem/MovieItemStyled.jsx
@@ -8,9 +8,15 @@ export const Ul = styled.ul`
 
 export const Li = styled.li`
   width: 225px;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.03);
+  }
 `;
 
 export const Img = styled.img`
+  border-radius: 10px;
   display: block;
   height: 100%;
   width: 100%;
